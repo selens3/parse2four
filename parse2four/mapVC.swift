9714fff3a3cfdfdf7415650156350a1594ee5d35
mehmet selen <mehmetselen2@yandex.com>
2018-10-24 21:50:08 +0300

    kullanıcının yerini bulmak tamam

--- parse2four/mapVC.swift
+++ parse2four/mapVC.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 import Parse
+import MapKit
 
-class mapVC: UIViewController {
+class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    
 
+    @IBOutlet weak var mapView: MKMapView!
+    var manager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
 
          
         
         
     }
     
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+        
+    }
+    
+    
     
 
+    @IBAction func saveClicked(_ sender: Any) {
+    }
+    
+    @IBAction func cancelClicked(_ sender: Any) {
+    }
+    
 }
