ce0fa077aee6c46243b5a972983f1f30498f8f27
mehmet selen <mehmetselen2@yandex.com>
2018-10-24 15:19:02 +0300

    kullanıcı çıkış işlemleri tamam

--- parse2four/placesVC.swift
+++ parse2four/placesVC.swift
@@ -11,13 +11,27 @@ import Parse
 
 class placesVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func logoutClicked(_ sender: Any) {
+        UserDefaults.standard.removeObject(forKey: "userLoggedIn")
+        UserDefaults.standard.synchronize()
+        
+        
+        let signInVC = self.storyboard?.instantiateViewController(withIdentifier: "signinVC") as! signInvc
+        let delegate : AppDelegate = UIApplication.shared.delegate as! AppDelegate
+        delegate.window?.rootViewController = signInVC
+        delegate.rememberLogIn()
+    }
+    
+    @IBAction func addClicked(_ sender: Any) {
+    }
     
 
 }
