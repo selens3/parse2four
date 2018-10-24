2a4c32bb77cbb0aa87414602edc9dc9230281608
mehmet selen <mehmetselen2@yandex.com>
2018-10-24 11:17:02 +0300

    Initial Commit

---
+++ parse2four/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parse2four
+//
+//  Created by Suat Selen on 24.10.2018.
+//  Copyright © 2018 mehmet selen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
