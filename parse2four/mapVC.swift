f75a289b0e8335c3c7115a37f707251fa099affc
mehmet selen <mehmetselen2@yandex.com>
2018-10-26 12:23:01 +0300

    yerleri kaydetmek tamam

--- parse2four/mapVC.swift
+++ parse2four/mapVC.swift
@@ -80,9 +80,36 @@ class mapVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
     
 
     @IBAction func saveClicked(_ sender: Any) {
+        let object = PFObject(className: "Places")
+        object["name"] = globalName
+        object["type"] = globalType
+        object["atmosphere"] = globalAtmosphere
+        object["latitude"] = self.choosenLatitude
+        object["longitude"] = self.choosenLongitude
+        
+        if let imageData = globalImage.jpegData(compressionQuality: 0.5) {
+            object["image"] = PFFile(name: "image.jpg", data: imageData)
+            
+        }
+        object.saveInBackground { (success, error) in
+            if error != nil {
+                let alert = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: UIAlertController.Style.alert)
+                let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.cancel, handler: nil)
+                alert.addAction(okButton)
+                self.present(alert, animated: true, completion: nil)
+                
+            } else {
+                self.performSegue(withIdentifier: "frommapVCtoplacesVC", sender: nil)
+                
+            }
+        }
+        
+        
     }
     
     @IBAction func cancelClicked(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+        
     }
     
 }
